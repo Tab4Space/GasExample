bdf791798e0db0bd7fa3bc50e66d512f125b88e4
Park Dong Hyeok <skypia0906@gmail.com>
2023-09-30 17:46:17 +0900

    23. Init Ability Actor Info

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -17,10 +17,12 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 
 public:
 	AAuraEnemy();
-	
+
+	/* Enemy Interface */
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
-
-	UPROPERTY(BlueprintReadOnly)
-	bool bHighlighted = false;
+	/* Enemy Interface */
+	
+protected:
+	virtual void BeginPlay() override;
 };
