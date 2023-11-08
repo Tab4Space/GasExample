5dab40a9844da6b9137e48b9d080a5b3f07a3bad
Park Dong Hyeok <skypia0906@gmail.com>
2023-11-08 17:56:18 +0900

    73. PlayerLevel and CombatInterface

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -22,8 +22,16 @@ public:
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
 	/* Enemy Interface */
+
+	/* Combat Interface */
+	virtual int32 GetPlayerLevel() override;
+	/* Combat Interface */
 	
 protected:
 	virtual void BeginPlay() override;
 	virtual void InitAbilityActorInfo() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Character Class Defaults")
+	int32 Level = 1;
+
 };
