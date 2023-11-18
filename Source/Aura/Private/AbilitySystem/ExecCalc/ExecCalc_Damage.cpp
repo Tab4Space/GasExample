3cd03af25fb954e182ab0c5ce19da27c57f9c4a8
Park Dong Hyeok <skypia0906@gmail.com>
2023-11-18 12:01:41 +0900

    143. Damage Execution Calculation

---
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -0,0 +1,28 @@
+// Copyright Tab4Space
+
+
+#include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
+
+#include "AbilitySystemComponent.h"
+
+UExecCalc_Damage::UExecCalc_Damage()
+{
+	
+}
+
+void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	/*
+	 * Decide how effect this calculation to other attribute
+	 *This calc contain GE
+	 */
+	const UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
+	const UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+
+	const AActor* SourceAvatar = SourceASC ? SourceASC->GetAvatarActor() : nullptr;
+	const AActor* TargetAvatar = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
+
+	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+
+	
+}
---
+++ Source/Aura/Public/AbilitySystem/ExecCalc/ExecCalc_Damage.h
@@ -0,0 +1,21 @@
+// Copyright Tab4Space
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "ExecCalc_Damage.generated.h"
+
+/**
+ * This is Custom Calculation Class when apply GE
+ */
+UCLASS()
+class AURA_API UExecCalc_Damage : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+
+public:
+	UExecCalc_Damage();
+
+	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
+};
