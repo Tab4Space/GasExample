8dd3319290539ca92bc5ced9e4dffc392fcaa996
Park Dong Hyeok <skypia0906@gmail.com>
2023-11-18 12:39:17 +0900

    144. ExecCalcs - Capturing Attributes

--- Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -4,10 +4,30 @@
 #include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
 
 #include "AbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+// raw c++ struct, not use 'F' prefix
+struct AuraDamageStatics
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	
+	AuraDamageStatics()
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target, false);		// not snapshot
+	}
+};
+
+static const AuraDamageStatics& DamageStatics()
+{
+	// create once, after reuse DStatics
+	static AuraDamageStatics DStatics;
+	return DStatics;
+}
 
 UExecCalc_Damage::UExecCalc_Damage()
 {
-	
+	// Add in capture list
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
@@ -24,5 +44,23 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 
 	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
 
+	// Gather tags from source and target
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	// Parameters for capture to pass function
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+
+	float Armor = 0.f;
 	
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluationParameters, Armor);
+
+	Armor = FMath::Max<float>(0.f, Armor);
+	++Armor;
+
+	// modify armor attribute
+	const FGameplayModifierEvaluatedData EvaluateData(DamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor);
+	OutExecutionOutput.AddOutputModifier(EvaluateData);
 }
