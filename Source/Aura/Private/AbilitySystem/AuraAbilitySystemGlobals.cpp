e6fc0ae318df2b5be8bc52eb9b6736db35a1450b
Park Dong Hyeok <skypia0906@gmail.com>
2023-11-20 16:52:53 +0900

    154. Aura Ability System Globals

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,12 @@
+// Copyright Tab4Space
+
+
+#include "AbilitySystem/AuraAbilitySystemGlobals.h"
+
+#include "AuraAbilityTypes.h"
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	
+	return new FAuraGameplayEffectContext;
+}
