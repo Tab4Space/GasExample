e331b318ec22d1570a0976e43f0ebe059dec0acd
Park Dong Hyeok <skypia0906@gmail.com>
2023-11-29 10:07:31 +0900

    219. Shaman Summon Locations

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
@@ -0,0 +1,30 @@
+// Copyright Tab4Space
+
+
+#include "AbilitySystem/Abilities/AuraSummonAbility.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
+{
+	const FVector Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector();
+	const FVector Location = GetAvatarActorFromActorInfo()->GetActorLocation();
+	const float DeltaSpread = SpawnSpread / NumMinions;		// angle between each vectors to spawn minion
+
+	const FVector LeftOfSpread = Forward.RotateAngleAxis(-SpawnSpread / 2.f, FVector::UpVector);
+	TArray<FVector> SpawnLocations;
+	
+	for(int32 i=0; i<NumMinions; i++)
+	{
+		const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread*i, FVector::UpVector);
+		const FVector ChosenSpawnLocation = Location + Direction * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
+		SpawnLocations.Add(ChosenSpawnLocation);
+		
+		DrawDebugSphere(GetWorld(), ChosenSpawnLocation, 20.f, 12, FColor::Cyan, false, 3.f);
+		UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), Location, Location+Direction*MaxSpawnDistance, 4.f, FLinearColor::Green, 3.f);
+		DrawDebugSphere(GetWorld(), Location+Direction*MinSpawnDistance, 5.f, 12, FColor::Red, false, 3.f);
+		DrawDebugSphere(GetWorld(), Location+Direction*MaxSpawnDistance, 5.f, 12, FColor::Red, false, 3.f);
+	}
+	
+	return SpawnLocations;
+}
--- Source/Aura/Public/Interaction/CombatInterface.h
+++ Source/Aura/Public/Interaction/CombatInterface.h
@@ -14,7 +14,7 @@ USTRUCT(BlueprintType)
 struct FTaggedMontage
 {
 	GENERATED_BODY()
-
+	
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	UAnimMontage* Montage = nullptr;
 
