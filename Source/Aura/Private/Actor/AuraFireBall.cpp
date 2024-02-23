092b80302cc009eeec35e7cac0de17b045d95d5e
Park Dong Hyeok <skypia0906@gmail.com>
2024-02-23 16:35:37 +0900

    368. Aura Fire Ball

---
+++ Source/Aura/Private/Actor/AuraFireBall.cpp
@@ -0,0 +1,16 @@
+// Copyright Tab4Space
+
+
+#include "Actor/AuraFireBall.h"
+
+void AAuraFireBall::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AAuraFireBall::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                                    const FHitResult& SweepResult)
+{
+	
+}
