7d661b708bd889ed38644da5158f903f2949bb27
Park Dong Hyeok <skypia0906@gmail.com>
2024-02-23 17:57:44 +0900

    371. Causing FireBall Damage

--- Source/Aura/Private/Actor/AuraProjectile.cpp
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -74,17 +74,7 @@ void AAuraProjectile::Destroyed()
 void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
                                       const FHitResult& SweepResult)
 {
-	if(DamageEffectParams.SourceAbilitySystemComponent == nullptr)
-	{
-		return;
-	}
-	AActor* SourceAvatarActor = DamageEffectParams.SourceAbilitySystemComponent->GetAvatarActor();
-	
-	if (SourceAvatarActor == OtherActor)
-	{
-		return;
-	}
-	if(!UAuraAbilitySystemLibrary::IsNotFriend(SourceAvatarActor, OtherActor))
+	if(!IsValidOverlap(OtherActor))
 	{
 		return;
 	}
@@ -121,4 +111,24 @@ void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, 
 	{
 		bHit = true;
 	}
+}
+
+bool AAuraProjectile::IsValidOverlap(AActor* OtherActor)
+{
+	if(DamageEffectParams.SourceAbilitySystemComponent == nullptr)
+	{
+		return false;
+	}
+	AActor* SourceAvatarActor = DamageEffectParams.SourceAbilitySystemComponent->GetAvatarActor();
+	
+	if (SourceAvatarActor == OtherActor)
+	{
+		return false;
+	}
+	if(!UAuraAbilitySystemLibrary::IsNotFriend(SourceAvatarActor, OtherActor))
+	{
+		return false;
+	}
+
+	return true;
 }
