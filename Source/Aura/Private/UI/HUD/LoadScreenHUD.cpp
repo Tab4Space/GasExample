e32afe679e881e9ec6b1d0ee4bc793ccec9538b5
Park Dong Hyeok <skypia0906@gmail.com>
2024-03-20 18:28:54 +0900

    386. Load Slot View Model

--- Source/Aura/Private/UI/HUD/LoadScreenHUD.cpp
+++ Source/Aura/Private/UI/HUD/LoadScreenHUD.cpp
@@ -12,9 +12,9 @@ void ALoadScreenHUD::BeginPlay()
 	Super::BeginPlay();
 
 	LoadScreenViewModel = NewObject<UMVVM_LoadScreen>(this, LoadScreenViewModelClass);
+	LoadScreenViewModel->InitializeLoadSlot();
 
 	LoadScreenWidget = CreateWidget<ULoadScreenWidget>(GetWorld(), LoadScreenWidgetClass);
 	LoadScreenWidget->AddToViewport();
-
-	
+	LoadScreenWidget->BlueprintInitializeWidget();
 }
---
+++ Source/Aura/Private/UI/ViewModel/MVVM_LoadSlot.cpp
@@ -0,0 +1,5 @@
+// Copyright Tab4Space
+
+
+#include "UI/ViewModel/MVVM_LoadSlot.h"
+
