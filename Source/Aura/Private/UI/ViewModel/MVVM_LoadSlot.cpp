f5d968dfb921e65ae308357e4fb8fc0b66603b44
Park Dong Hyeok <skypia0906@gmail.com>
2024-03-21 17:35:39 +0900

    387. Switching the Widget Switcher

--- Source/Aura/Private/UI/ViewModel/MVVM_LoadSlot.cpp
+++ Source/Aura/Private/UI/ViewModel/MVVM_LoadSlot.cpp
@@ -3,3 +3,10 @@
 
 #include "UI/ViewModel/MVVM_LoadSlot.h"
 
+void UMVVM_LoadSlot::InitializeSlot()
+{
+	// TODO: Check slot status based on loaded data.
+	SetWidgetSwitcherIndex.Broadcast(1);
+
+	
+}
