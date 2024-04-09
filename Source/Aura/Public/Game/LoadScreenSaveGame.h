c92b29638b4aad4e2c9864c0de4e58e9131718f8
Park Dong Hyeok <skypia0906@gmail.com>
2024-04-09 18:33:04 +0900

    418. Map Entrance

--- Source/Aura/Public/Game/LoadScreenSaveGame.h
+++ Source/Aura/Public/Game/LoadScreenSaveGame.h
@@ -99,6 +99,9 @@ public:
 	UPROPERTY();
 	FString MapName = FString("Default Map Name");
 
+	UPROPERTY();
+	FString MapAssetName = FString("Default Map Asset Name");
+
 	UPROPERTY()
 	FName PlayerStartTag;
 
