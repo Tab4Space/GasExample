e0fa601308e263feb6c1bf4d7c454273ae7c2f72
Park Dong Hyeok <skypia0906@gmail.com>
2024-04-10 16:18:27 +0900

    422. Spawn Volumes

---
+++ Source/Aura/Public/Actor/AuraEnemySpawnPoint.h
@@ -0,0 +1,31 @@
+// Copyright Tab4Space
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+#include "Engine/TargetPoint.h"
+#include "AuraEnemySpawnPoint.generated.h"
+
+class AAuraEnemy;
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraEnemySpawnPoint : public ATargetPoint
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void SpawnEnemy();
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Enemy")
+	TSubclassOf<AAuraEnemy> EnemyClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Enemy")
+	int32 EnemyLevel = 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Enemy")
+	ECharacterClass CharacterClass = ECharacterClass::Warrior;
+};
